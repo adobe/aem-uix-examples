46c5e56bbc6d2328ed196907ab4415e3f4190eef
Valerii Naida <vnayda@adobe.com>
2023-09-19 10:40:00 -0500

    feat(SITES-16080): US Address Autocomplete Custom Element

---
+++ address-autocomplete/src/aem-cf-editor-1/web-src/src/components/App.js
@@ -0,0 +1,44 @@
+/*
+ * <license header>
+ */
+
+import React from "react";
+import ErrorBoundary from "react-error-boundary";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import ExtensionRegistration from "./ExtensionRegistration";
+
+function App() {
+  return (
+    <Router>
+      <ErrorBoundary onError={onError} FallbackComponent={fallbackComponent}>
+        <Routes>
+          <Route index element={<ExtensionRegistration />} />
+          <Route
+            exact path="index.html"
+            element={<ExtensionRegistration />}
+          />
+          {/* @todo YOUR CUSTOM ROUTES SHOULD BE HERE */}
+        </Routes>
+      </ErrorBoundary>
+    </Router>
+  )
+
+  // Methods
+
+  // error handler on UI rendering failure
+  function onError(e, componentStack) {}
+
+  // component to show if UI fails rendering
+  function fallbackComponent({ componentStack, error }) {
+    return (
+      <React.Fragment>
+        <h1 style={{ textAlign: "center", marginTop: "20px" }}>
+          Phly, phly... Something went wrong :(
+        </h1>
+        <pre>{componentStack + "\n" + error.message}</pre>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ address-autocomplete/src/aem-cf-editor-1/web-src/src/components/ExtensionRegistration.js
@@ -0,0 +1,22 @@
+/*
+ * <license header>
+ */
+
+import { Text } from "@adobe/react-spectrum";
+import { register } from "@adobe/uix-guest";
+import { extensionId } from "./Constants";
+
+function ExtensionRegistration() {
+  const init = async () => {
+    const guestConnection = await register({
+      id: extensionId,
+      methods: {
+      }
+    });
+  };
+  init().catch(console.error);
+
+  return <Text>IFrame for integration with Host (AEM)...</Text>;
+}
+
+export default ExtensionRegistration;
