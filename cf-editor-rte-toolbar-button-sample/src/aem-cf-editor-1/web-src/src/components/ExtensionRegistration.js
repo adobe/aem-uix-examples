046b2575a40cb73fa5036f127a82cab2b28fe6a0
Serhii Litviachenko <slitviachenko@gmail.com>
2023-12-19 14:45:33 -0600

    CR changes

--- cf-editor-rte-toolbar-button-sample/src/aem-cf-editor-1/web-src/src/components/ExtensionRegistration.js
+++ cf-editor-rte-toolbar-button-sample/src/aem-cf-editor-1/web-src/src/components/ExtensionRegistration.js
@@ -1,6 +1,13 @@
 /*
- * <license header>
- */
+Copyright 2023 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 
 import { Text } from "@adobe/react-spectrum";
 import { register } from "@adobe/uix-guest";
@@ -46,7 +53,7 @@ function ExtensionRegistration() {
             // RTE Toolbar custom button
             {
               id: "aem-extensibility-cf-editor-rte-toolbar-button",
-              tooltip: "Unlock the power of extensibility in the Content Fragment Editor - explore enhanced text customization with a custom toolbar button for the Rich Text Editor",
+              tooltip: "Click to explore the extensibility features",
               icon: "Plug",
               onClick: onClickHandler(),
             },
--- cf-editor-rte-toolbar-button-sample/src/aem-cf-editor-1/web-src/src/utils.js
+++ cf-editor-rte-toolbar-button-sample/src/aem-cf-editor-1/web-src/src/utils.js
@@ -1,5 +1,12 @@
 /*
-* <license header>
+Copyright 2023 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
 */
 
 /* global fetch */
@@ -16,7 +23,7 @@
  *
  */
 
-async function actionWebInvoke (actionUrl, headers = {}, params = {}, options = { method: 'POST' }) {  
+async function actionWebInvoke (actionUrl, headers = {}, params = {}, options = { method: 'POST' }) {
   const actionHeaders = {
     'Content-Type': 'application/json',
     ...headers
@@ -38,11 +45,11 @@ async function actionWebInvoke (actionUrl, headers = {}, params = {}, options = 
   } else if (fetchConfig.method === 'POST') {
     fetchConfig.body = JSON.stringify(params)
   }
-  
+
   const response = await fetch(actionUrl, fetchConfig)
 
   let content = await response.text()
-  
+
   if (!response.ok) {
     return JSON.parse(content)
   }
