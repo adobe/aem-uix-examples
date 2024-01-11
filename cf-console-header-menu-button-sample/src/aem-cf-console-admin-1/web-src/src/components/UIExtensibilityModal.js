adbbb6f8b97745e8789169a3616a3c082a0b5660
Serhii Litviachenko <slitviachenko@gmail.com>
2024-01-11 14:35:36 -0600

    Remove Progress Circle

--- cf-console-header-menu-button-sample/src/aem-cf-console-admin-1/web-src/src/components/UIExtensibilityModal.js
+++ cf-console-header-menu-button-sample/src/aem-cf-console-admin-1/web-src/src/components/UIExtensibilityModal.js
@@ -15,20 +15,24 @@ import {
   Heading,
   Provider,
   Content,
+  Divider,
   defaultTheme,
   Button,
   View
 } from "@adobe/react-spectrum";
+import "./UIExtensibilityModal.css";
 
 import { extensionId } from "./Constants";
 
 export default UIExtensibilityModal = () => {
-  const [guestConnection, setGuestConnection] = useState();
+  const [guestConnection, setGuestConnection] = useState(null);
+  const [sharedContext, setSharedContext] = useState(null);
 
   useEffect(() => {
     const init = async () => {
       const connection = await attach({ id: extensionId });
       setGuestConnection(connection);
+      setSharedContext(connection.sharedContext);
     };
 
     init().catch(console.error);
@@ -42,11 +46,6 @@ export default UIExtensibilityModal = () => {
     });
   }, [guestConnection]);
 
-  const displayProgressCircle = useCallback((e) => {
-    guestConnection.host.progressCircle.start();
-    setTimeout(() => guestConnection.host.progressCircle.stop(), 1500);
-  }, [guestConnection]);
-
   const onCloseHandler = () => {
     guestConnection.host.modal.close();
   };
@@ -54,7 +53,8 @@ export default UIExtensibilityModal = () => {
   return (
     <Provider theme={defaultTheme} colorScheme="light">
       <Content width="100%">
-        <Heading level={4}>These are some of AEM UI Extensibility features</Heading>
+        <Heading level={3}>These are some of AEM UI Extensibility features</Heading>
+        <Divider size="M" marginBottom="size-200" />
         <View marginBottom="size-150">
           <Button
             onPress={(e) => displayToast("Toast displayed successfully!")}
@@ -63,13 +63,31 @@ export default UIExtensibilityModal = () => {
             Show toast
           </Button>
         </View>
-        <View>
-          <Button
-            onPress={displayProgressCircle}
-            variant="primary"
-          >
-            Show Progress Circle
-          </Button>
+        {(sharedContext !== null) &&
+          <View marginBottom="size-150">
+            <Divider size="S" />
+            <Heading level={4} marginTop="size-85" marginBottom="size-85">Shared Context</Heading>
+            <pre className="shared-context">
+              {`
+{
+  aemHost: "${sharedContext.get("aemHost")}", // string, hostname of connected AEM environment
+  locale: "${sharedContext.get("locale")}", // string, locale of current user
+  theme: "${sharedContext.get("theme")}", // "light" | "dark", color schema selected by current user
+  auth: {
+    imsOrg: "${sharedContext.get("auth").imsOrg}", // string, current IMS organization
+    imsOrgName: "${sharedContext.get("auth").imsOrgName}", // string, human readable organization name
+    apiKey: "${sharedContext.get("auth").apiKey}", // string, API key to use for requests to Adobe services
+    authScheme: "${sharedContext.get("auth").authScheme}", // "Bearer", auth schema that should be used during communication with Adobe services
+    imsToken: "\${sharedContext.get("auth").imsToken}" // string, user token (not shown here for security reasons)
+  }
+}
+              `}
+            </pre>
+          </View>
+        }
+        <Divider size="S" marginBottom="size-150" />
+        <View UNSAFE_className="more-ui-features-notice">
+          More UI Extensibility features coming soon ...
         </View>
 
         <Button variant="primary" onPress={onCloseHandler} position="fixed" bottom="0px" right="8px">
