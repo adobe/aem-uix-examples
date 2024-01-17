2802f9700c7fe1d8c5cb15f5ec0c946454faeaf1
Serhii Litviachenko <litviach@adobe.com>
2024-01-17 11:57:33 -0600

    CF Console Header Menu Button Sample Extension (#11)

---
+++ cf-console-header-menu-button-sample/src/aem-cf-console-admin-1/web-src/src/components/ExtensionRegistration.js
@@ -0,0 +1,74 @@
+/*
+Copyright 2024 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import { Text } from "@adobe/react-spectrum";
+import { register } from "@adobe/uix-guest";
+import { extensionId } from "./Constants";
+
+function ExtensionRegistration() {
+  const init = async () => {
+    const guestConnection = await register({
+      id: extensionId,
+      methods: {
+        headerMenu: {
+          getButtons() {
+            return [
+              // YOUR HEADER BUTTONS CODE SHOULD BE HERE
+              {
+                id: "aem-uix-examples-cf-console-header-menu-button",
+                label: "Sample Button",
+                icon: "Plug",
+                variant: "secondary",
+                subItems: [
+                  {
+                    id: "aem-uix-examples-cf-console-header-menu-button-weather-forecast",
+                    label: "Weather Forecast",
+                    icon: "Cloud",
+                    onClick: () => {
+                      const modalURL = "/index.html#/aem-uix-examples-cf-console-header-menu-button-weather-forecast";
+
+                      guestConnection.host.modal.showUrl({
+                        title: "Weather Forecast",
+                        url: modalURL,
+                        height: "320px",
+                        width: "550px",
+                      });
+                    },
+                  },
+                  {
+                    id: "aem-uix-examples-cf-console-header-menu-button-ui-extensibility",
+                    label: "Developer Tools",
+                    icon: "Hammer",
+                    onClick: () => {
+                      const modalURL = "/index.html#/aem-uix-examples-cf-console-header-menu-button-ui-extensibility";
+
+                      guestConnection.host.modal.showUrl({
+                        title: "Developer Tools",
+                        url: modalURL,
+                        height: "250px",
+                        width: "550px",
+                      });
+                    },
+                  },
+                ],
+              },
+            ];
+          },
+        },
+      },
+    });
+  };
+  init().catch(console.error);
+
+  return <Text>IFrame for integration with Host (AEM)...</Text>
+}
+
+export default ExtensionRegistration;
