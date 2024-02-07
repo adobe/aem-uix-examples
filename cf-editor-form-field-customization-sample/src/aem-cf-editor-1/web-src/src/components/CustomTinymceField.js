d22a92e7886ddd8da1484b8618352b4aeba53e9d
Serhii Litviachenko <slitviachenko@gmail.com>
2024-02-07 17:42:43 -0600

    Switch to bundling TinyMCE

--- cf-editor-form-field-customization-sample/src/aem-cf-editor-1/web-src/src/components/CustomTinymceField.js
+++ cf-editor-form-field-customization-sample/src/aem-cf-editor-1/web-src/src/components/CustomTinymceField.js
@@ -22,6 +22,55 @@ import {
 import { Label } from "@react-spectrum/label";
 import { Editor } from "@tinymce/tinymce-react";
 
+// see https://www.tiny.cloud/docs/tinymce/6/react-pm-bundle/
+import tinymce from "tinymce/tinymce";
+// DOM model
+import "tinymce/models/dom/model";
+// Theme
+import "tinymce/themes/silver";
+// Toolbar icons
+import "tinymce/icons/default";
+// Editor styles
+import "tinymce/skins/ui/oxide/skin.min.css";
+// importing plugins
+// if you use a plugin that is not listed here the editor will fail to load
+import "tinymce/plugins/advlist";
+import "tinymce/plugins/anchor";
+import "tinymce/plugins/autolink";
+import "tinymce/plugins/autoresize";
+import "tinymce/plugins/autosave";
+import "tinymce/plugins/charmap";
+import "tinymce/plugins/code";
+import "tinymce/plugins/codesample";
+import "tinymce/plugins/directionality";
+import "tinymce/plugins/emoticons";
+import "tinymce/plugins/fullscreen";
+import "tinymce/plugins/help";
+import "tinymce/plugins/image";
+import "tinymce/plugins/importcss";
+import "tinymce/plugins/insertdatetime";
+// replace the original ./node_modules/tinymce/plugins/link/plugin.js plugin with our modified version
+// import "tinymce/plugins/link";
+import "../../../../../patches/tinymce/plugins/link";
+import "tinymce/plugins/lists";
+import "tinymce/plugins/media";
+import "tinymce/plugins/nonbreaking";
+import "tinymce/plugins/pagebreak";
+import "tinymce/plugins/preview";
+import "tinymce/plugins/quickbars";
+import "tinymce/plugins/save";
+import "tinymce/plugins/searchreplace";
+import "tinymce/plugins/table";
+import "tinymce/plugins/template";
+import "tinymce/plugins/visualblocks";
+import "tinymce/plugins/visualchars";
+import "tinymce/plugins/wordcount";
+// importing plugin resources
+import "tinymce/plugins/emoticons/js/emojis";
+// Content styles, including inline UI like fake cursors
+import contentCss from "tinymce/skins/content/default/content.min.css";
+import contentUiCss from "tinymce/skins/ui/oxide/content.min.css";
+
 const plugins = [
   "advlist autolink lists link image charmap preview anchor",
   "searchreplace visualblocks code",
@@ -103,6 +152,8 @@ const CustomTinymceField = () => {
     guestConnection.host.field.onChange(newValue);
   };
 
+  // note that skin and content_css is disabled to avoid the normal
+  // loading process and is instead loaded as a string via content_style
   return (
     <Provider theme={lightTheme} colorScheme="light">
       {fieldData.ready &&
@@ -111,8 +162,12 @@ const CustomTinymceField = () => {
             {fieldData.fieldLabel}
           </Label>
           <Editor
-            tinymceScriptSrc="/tinymce.min.js"
-            init={defaultConfig}
+            init={{
+              ...defaultConfig,
+              skin: false,
+              content_css: false,
+              content_style: [contentCss, contentUiCss].join("\n"),
+            }}
             initialValue={fieldData.value}
             onInit={(evt, editor) => editorRef.current = editor}
             onEditorChange={onEditorChangeHandler}
