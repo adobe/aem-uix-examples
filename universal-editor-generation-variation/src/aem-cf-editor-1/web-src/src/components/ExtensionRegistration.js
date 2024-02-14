5bf102f7b1e4353cdfdae49af632959d74786d6c
Felix Delval <fdelval@adobe.com>
2024-02-14 10:39:50 +0100

    Data from template

---
+++ universal-editor-generation-variation/src/aem-cf-editor-1/web-src/src/components/ExtensionRegistration.js
@@ -0,0 +1,37 @@
+/*
+ * <license header>
+ */
+
+import { Text } from "@adobe/react-spectrum";
+import { register } from "@adobe/uix-guest";
+import { extensionId } from "./Constants";
+
+function ExtensionRegistration() {
+  const init = async () => {
+    const guestConnection = await register({
+      id: extensionId,
+      methods: {
+        headerMenu: {
+          getButtons() {
+            return [
+              // @todo YOUR HEADER BUTTONS DECLARATION SHOULD BE HERE
+              {
+                id: 'variation-generation',
+                label: 'Variation Generation',
+                icon: 'OpenIn',
+                onClick() {
+                  console.log("Button 'variation-generation' has been pressed.");
+                },
+              },
+            ];
+          },
+        },
+      }
+    });
+  };
+  init().catch(console.error);
+
+  return <Text>IFrame for integration with Host (AEM)...</Text>;
+}
+
+export default ExtensionRegistration;
