dde1f96f5b6d1166b71b93776bfbaad6b739d36f
Vladimir Zaets <vzaets@adobe.com>
2024-08-15 23:21:38 -0500

    cf-editor-custom-rte-field

---
+++ cf-editor-custom-rte-field/src/aem-cf-editor-1/web-src/src/components/ExtensionRegistration.js
@@ -0,0 +1,37 @@
+/*
+Copyright 2023 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import { Text } from "@adobe/react-spectrum";
+import { register } from "@adobe/uix-guest";
+import { extensionId } from "./Constants";
+
+function ExtensionRegistration() {
+  const init = async () => {
+    const guestConnection = await register({
+      id: extensionId,
+      methods: {
+        field: {
+          getDefinitions: () => ([
+            {
+              fieldNameExp: "^singleMultilineText",
+              url: "/#/custom-tinymce?characterLimit=100&characterLimitEnforcement=true",
+            },
+          ]),
+        }
+      }
+    });
+  };
+  init().catch(console.error);
+
+  return <Text>IFrame for integration with Host (AEM)...</Text>;
+}
+
+export default ExtensionRegistration;
